bb419aa25717f0b1c4003087dcdd6ed33a038ca8
Alwin Arrasyid <alwin.ridd@gmail.com>
2018-05-08 15:10:57 +0700

    Initial commit.

---
+++ src/transform.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function versionToInteger(major, minor, patch) {
+  const x = Math.pow(2, major);
+  const y = Math.pow(3, minor);
+  const z = Math.pow(5, patch);
+
+  return x * y * z;
+}
+
+function versionStringToInteger(version) {
+  const [major, minor, patch] = version.split(`.`);
+
+  return semverToInteger()
+}
+
+function getPrimeFactors(number) {
+  const result = [];
+  let num = number;
+  let div = 2;
+
+  while (num !== 0) {
+    if (num % div !== 0) {
+      div += 1;
+    } else {
+      num /= div;
+      result.push(div);
+
+      if (num === 1) {
+        break;
+      }
+    }
+  }
+
+  return result;
+}
+
+function integerToVersionString(versionInteger) {
+  let twos = 0;
+  let threes = 0;
+  let fives = 0;
+
+  const factors = getPrimeFactors(versionInteger);
+  factors.forEach((factor) => {
+    if (factor === 2) {
+      twos += 1;
+    } else if (factor === 3) {
+      threes += 1;
+    } else if (factor === 5) {
+      fives += 1;
+    }
+  });
+
+  return `${twos}.${threes}.${fives}`;
+}
+
+module.exports = {
+  versionToInteger,
+  versionStringToInteger,
+  integerToVersionString,
+};
+
